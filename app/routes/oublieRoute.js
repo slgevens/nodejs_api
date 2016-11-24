446aaea7a9674bc72febe13e7c7857c999f13107
Evens <evenssolignac@live.fr>
2016-11-24 22:09:02 +0100

    add route oublie

---
+++ app/routes/oublieRoute.js
@@ -0,0 +1,24 @@
+var mysql = require('mysql');
+
+module.exports = function(router, connection) {
+    router.route('/oublie')
+        .post(function(req, res){
+	   
+	    var query = "SELECT ?? FROM ?? WHERE ?? = ? ";
+	    var table = ['ID_USER', 'photo_expresso.users', 'MAIL', req.body.mail ];
+	    
+	    query = mysql.format(query, table);
+	    connection.query(query, function(err, result){
+		if (err)
+		    res.sendStatus(400);
+		else
+		    if(result[0]){
+			console.log("bon user");
+		    }else{
+			console.log("bad user");
+		    }
+		res.status(201).send("envoi");
+		    console.log(result[0]);
+	    });
+	})
+};
