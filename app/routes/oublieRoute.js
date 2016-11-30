78aef453d1c82643e9f5be62cba7216f927eb14c
Evens <evenssolignac@live.fr>
2016-11-30 19:12:26 +0100

    working route

--- app/routes/oublieRoute.js
+++ app/routes/oublieRoute.js
@@ -26,23 +26,31 @@ module.exports = function(router, connection) {
 	    connection.query(query, function(err, result){
 		if (err) {
 		    res.status(400);
+		    console.log('11111111111111111111111111111111111111111111111111');
 		}		
 		else {
-		    if(result.length != 0) {	
+		    console.log(result);
+		    if(result.length != 0) {
+			console.log('22222222222222222222222222222222222222222222222')
 			// change password if the mail exists and cleaning token and token_validity
 			var new_token = '';
 			var update_passsword = "UPDATE ?? SET ?? = ?, ?? = ?, ?? = ? WHERE ?? = ?";
-			var table_update = ['photo_expresso.login', 'PASSWORD', randompass(10), 'TOKEN', new_token, 'TOKEN_VALIDITY', new_token, 'ID_LOGIN', result[0] ];
+			var table_update = ['photo_expresso.login', 'PASSWORD', randompass(10), 'TOKEN', new_token,
+					    'TOKEN_VALIDITY', new_token, 'ID_LOGIN', result[0] ];
 			update_passsword = mysql.format(update_passsword, table_update);
 
 			connection.query(update_passsword, function(err, result2){
-			if(err){
+			    if(err){
+				console.log('33333333333333333333333');
 			    res.status(400);
 			}
-			else {
+			    else {
+				console.log('44444444444444444444444');
 			    res.status(200);
 			}
 			});
+		    } else {
+			res.status(404).send("Nothing to show");
 		    }
 		}
 	    });
