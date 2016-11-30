d4b54f56ae5086b1b432e56683000eda7a63e59c
Evens <evenssolignac@live.fr>
2016-11-30 18:41:15 +0100

    new if result exist

--- app/routes/oublieRoute.js
+++ app/routes/oublieRoute.js
@@ -25,29 +25,25 @@ module.exports = function(router, connection) {
 	    query = mysql.format(query, table);
 	    connection.query(query, function(err, result){
 		if (err) {
-		    res.sendStatus(400);
-		}
+		    res.status(400);
+		}		
 		else {
-		    if(result[0]){
+		    if(result.length != 0) {	
 			// change password if the mail exists and cleaning token and token_validity
 			var new_token = '';
 			var update_passsword = "UPDATE ?? SET ?? = ?, ?? = ?, ?? = ? WHERE ?? = ?";
 			var table_update = ['photo_expresso.login', 'PASSWORD', randompass(10), 'TOKEN', new_token, 'TOKEN_VALIDITY', new_token, 'ID_LOGIN', result[0] ];
 			update_passsword = mysql.format(update_passsword, table_update);
-			connection.query(update_passsword, function(errupdate, resultupdate){
-			    if(errupdate){
-				res.status(400);
-			    }
-			    else {
-				res.status(200);
-			    }
+
+			connection.query(update_passsword, function(err, result2){
+			if(err){
+			    res.status(400);
+			}
+			else {
+			    res.status(200);
+			}
 			});
-			res.status(200);
 		    }
-		    else {
-			res.status(400);
-		    }
-		    res.status(200); 
 		}
 	    });
 	})
