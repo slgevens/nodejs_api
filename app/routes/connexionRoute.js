5c7918f2a1cb3ebdec2ad4d067b9ffc4e05daeac
Evens <evenssolignac@live.fr>
2016-12-01 18:20:57 +0100

    crash when requested, maybbe a condition

--- app/routes/connexionRoute.js
+++ app/routes/connexionRoute.js
@@ -1,38 +1,39 @@
-// On apelle les dépendances comme dhab
 var mysql = require('mysql');
 var jwt   = require('jsonwebtoken');
 
-// un exports de module C.F les require dans le fichier server.js
 module.exports = function(router, connection) {
     router.route('/hello')
         .get(function(req, res) {
-            // Test de l'api xx.xx.xx/api/hello
-            res.status(200).send("Ok !");
+            res.status(200).send("Hello !");
         })
-    router.route('/login/')
+    router.route('/connexion')
 	.post(function(req, res){
 	    var query_1 = "SELECT * FROM ?? WHERE ?? = ? AND ?? = ?";
-	    var table_1 = ['photo_expresso.login', 'username', req.body.username, 'password', req.body.password];
+	    var table_1 = ['photo_expresso.login', 'MAIL', req.body.email, 'PASSWORD', req.body.password];
 
 	    query_1 = mysql.format(query_1, table_1);
-	    connection.query(query_1, function(err, result_1){
-		if (err)
+	    connection.query(query_1, function(err, result){
+		if (err) {
 		    res.status(400).send(err);
-		else if (result_1.length == 0)
-		    res.status(404).send("Not found !");
+		}
+		else if (result_1.length == 0) {
+		    res.status(404).send("Introuvable !");
+		}
 		else {
-		    if (req.body.password !== result_1[0].password)
-			res.status(401).send("Non authorized");
+		    if (req.body.password !== result_1[0].password) {			
+			res.status(401).send("Non autorisé !");
+		    }
 		    else {
-			console.log(req.body.password, result_1[0].password);
+			console.log('222222222222222222222222', req.body.password, result_1[0].password);
 			var query_2 = "SELECT * FROM ?? WHERE ?? = ?";
-			var table_2 = ['photo_expresso.view_users', 'ID_USER', result_1[0].id];
+			var table_2 = ['photo_expresso.usersInfos', 'ID_USER', result_1[0].id];
 			query_2 = mysql.format(query_2, table_2);
 			connection.query(query_2, function(err, result_2){
-			    if  (err)
+			    if  (err) {
 				res.status(400).send(err);
+			    }
 			    else {
-				var token = jwt.sign(result_2[0], "token_secret", {
+				var token = jwt.sign(result_2[0], "nv7D4ZzOQ7", {
 				    expiresIn:14400
 				});
 				res.send(token);
@@ -42,19 +43,5 @@ module.exports = function(router, connection) {
 		}
 	    })
 	    
-	})
-    router.route('/delete/:id')
-        .put(function(req, res){
-	    var query = "UPDATE ?? SET ?? = ? WHERE ?? = ?"
-	    var table = ['photo_expresso.login', 'is_archived', '1', 'id', req.params.id];
-	    query = mysql.format(query, table);
-	    connection.query(query, function(err) {
-		if (err) {
-		    res.status(400).send(err);
-		}
-		else {
-		    res.status(200).send("Deleted");
-		}
-	    })
 	})
 };
