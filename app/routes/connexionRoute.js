fcbf8b1e600722de55ccb447384a4eb251a107c6
Evens <evenssolignac@live.fr>
2016-12-05 21:21:33 +0100

    working connexion route

--- app/routes/connexionRoute.js
+++ app/routes/connexionRoute.js
@@ -8,25 +8,24 @@ module.exports = function(router, connection) {
         })
     router.route('/connexion')
 	.post(function(req, res){
-	    var query_1 = "SELECT * FROM ?? WHERE ?? = ? AND ?? = ?";
-	    var table_1 = ['photo_expresso.login', 'MAIL', req.body.email, 'PASSWORD', req.body.password];
+	    var query_1 = "SELECT * FROM ?? WHERE ?? = ? AND ?? = ? AND ?? = ?";
+	    var table_1 = ['photo_expresso.login', 'MAIL', req.body.email, 'PASSWORD', req.body.password, 'IS_ARCHIVED', 0];
 
 	    query_1 = mysql.format(query_1, table_1);
 	    connection.query(query_1, function(err, result){
 		if (err) {
 		    res.status(400).send(err);
 		}
-		else if (result_1.length == 0) {
+		else if (result.length == 0) {
 		    res.status(404).send("Introuvable !");
 		}
 		else {
-		    if (req.body.password !== result_1[0].password) {			
+		    if (req.body.password !== result[0].PASSWORD) {			
 			res.status(401).send("Non autorisé !");
 		    }
 		    else {
-			console.log('222222222222222222222222', req.body.password, result_1[0].password);
-			var query_2 = "SELECT * FROM ?? WHERE ?? = ?";
-			var table_2 = ['photo_expresso.usersInfos', 'ID_USER', result_1[0].id];
+			var query_2 = "SELECT ID_USER, MAIL, FIRSTNAME, LASTNAME FROM ?? WHERE ?? = ? AND ?? = ?";
+			var table_2 = ['photo_expresso.usersInfos', 'ID_USER', result[0].ID_USER, 'MAIL', req.body.email];
 			query_2 = mysql.format(query_2, table_2);
 			connection.query(query_2, function(err, result_2){
 			    if  (err) {
