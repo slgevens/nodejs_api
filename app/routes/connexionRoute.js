df61ee5d2937888c0e2a0caee6c570c07a9276dc
Evens <evenssolignac@live.fr>
2016-12-14 16:27:54 +0100

    working route

--- app/routes/connexionRoute.js
+++ app/routes/connexionRoute.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 var jwt   = require('jsonwebtoken');
+var pass = require('password-hash');
 
 module.exports = function(router, connection) {
     router.route('/hello')
@@ -8,24 +9,25 @@ module.exports = function(router, connection) {
         })
     router.route('/connexion')
 	.post(function(req, res){
-	    var query_1 = "SELECT * FROM ?? WHERE ?? = ? AND ?? = ? AND ?? = ?";
-	    var table_1 = ['photo_expresso.login', 'MAIL', req.body.email, 'PASSWORD', req.body.password, 'IS_ARCHIVED', 0];
+	    var query_1 = "SELECT * FROM ?? WHERE ?? = ? AND ?? = ?";
+	    var table_1 = ['photo_expresso.login', 'MAIL', req.body.email, 'IS_ARCHIVED', 0];	  
 
 	    query_1 = mysql.format(query_1, table_1);
 	    connection.query(query_1, function(err, result){
+
 		if (err) {
 		    res.status(400).send(err);
 		}
 		else if (result.length == 0) {
-		    res.status(404).send("Introuvable !");
+		    res.status(404).send("Adresse mail ou mot de passe introuvable !");
 		}
 		else {
-		    if (req.body.password !== result[0].PASSWORD) {			
-			res.status(401).send("Non autorisé !");
+		    if (!pass.verify(req.body.password, result[0].PASSWORD)) {
+			res.status(401).send("Adresse mail ou mot de passe incorrect !");
 		    }
 		    else {
 			var query_2 = "SELECT ID_USER, MAIL, FIRSTNAME, LASTNAME FROM ?? WHERE ?? = ? AND ?? = ?";
-			var table_2 = ['photo_expresso.usersInfos', 'ID_USER', result[0].ID_USER, 'MAIL', req.body.email];
+			var table_2 = ['photo_expresso.view_users_detail', 'ID_USER', result[0].ID_USER, 'MAIL', req.body.email];
 			query_2 = mysql.format(query_2, table_2);
 			connection.query(query_2, function(err, result_2){
 			    if  (err) {
