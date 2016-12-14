caa489d7edc8459fb9ef8f62817ff098aac1a95e
Evens <evenssolignac@live.fr>
2016-12-14 18:15:09 +0100

    changing database | settings

--- app/routes/fraisRoute.js
+++ app/routes/fraisRoute.js
@@ -3,8 +3,8 @@ var mysql = require('mysql');
 module.exports = function(router, connection) {
     router.route('/frais')
         .get(function(req, res){
-	    var query = "SELECT * FROM ?? WHERE ?? = ?";
-	    var table = ['photo_expresso.shipping', 'STATUS', '0'];
+	    var query = "SELECT * FROM ?? WHERE ?? LIKE '%SHIPPING_%' AND ?? = ?";
+	    var table = ['photo_expresso.settings', 'ID_TYPE', 'STATUS', '0'];
 
 	    query = mysql.format(query, table);
 	    connection.query(query, function(err, result){
