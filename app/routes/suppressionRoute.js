2815b91ef4d78d2fa012a1663d58fb879c0af124
Evens <evenssolignac@live.fr>
2016-12-15 02:52:38 +0100

    working route

--- app/routes/suppressionRoute.js
+++ app/routes/suppressionRoute.js
@@ -1,18 +1,20 @@
 var mysql = require('mysql');
-var crypto = require('crypto');
+var jwtDecode = require('jwt-decode');
 
 module.exports = function(router, connection) {
-    router.route('/suppression/:id')
+    router.route('/suppression/:id?')
 	.put(function(req, res){
+	    var decodeIdUser = jwtDecode(req.headers.authorization).ID_USER;
 	    var query = "UPDATE ?? SET ?? = ? WHERE ?? = ?"
-	    var table = ['photo_expresso.login', 'is_archived', '1', 'id', req.params.id];
+	    var table = ['photo_expresso.login', 'is_archived', '1', 'ID_USER', decodeIdUser];
+	    
 	    query = mysql.format(query, table);
 	    connection.query(query, function(err) {
 		if (err) {
 		    res.status(400).send(err);
 		}
 		else {
-		    res.status(201).send("Supprimé !");
+		    res.status(201).send("Compte supprimé !");
 		}
 	    })
 	})
