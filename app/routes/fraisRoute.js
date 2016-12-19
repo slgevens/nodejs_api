fdd5b80b4246bf5769691ad5ac81de98ef871ddd
Evens <evenssolignac@live.fr>
2016-12-19 16:53:17 +0100

    working route and mass change in bdd

--- app/routes/fraisRoute.js
+++ app/routes/fraisRoute.js
@@ -3,8 +3,8 @@ var mysql = require('mysql');
 module.exports = function(router, connection) {
     router.route('/frais')
         .get(function(req, res){
-	    var query = "SELECT * FROM ?? WHERE ?? LIKE '%SHIPPING_%' AND ?? = ?";
-	    var table = ['photo_expresso.settings', 'ID_TYPE', 'STATUS', '0'];
+	    var query = "SELECT * FROM ?? ";
+	    var table = ['photo_expresso.settings'];
 
 	    query = mysql.format(query, table);
 	    connection.query(query, function(err, result){
