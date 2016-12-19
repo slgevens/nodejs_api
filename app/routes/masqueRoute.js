de1ac2c1d4d9c8d9c3b902c0c432e2f620b3267c
Evens <evenssolignac@live.fr>
2016-12-19 16:35:30 +0100

    maque route working

--- app/routes/masqueRoute.js
+++ app/routes/masqueRoute.js
@@ -3,17 +3,15 @@ var mysql = require('mysql');
 module.exports = function(router, connection) {
     router.route('/masque')
 	.get(function(req, res){
-	    var query = "SELECT * FROM ?? WHERE ?? LIKE '%MASQUE_%' AND ?? = ?";
-	    var table = ['photo_expresso.settings', 'ID_TYPE', 'STATUS', '0'];
+	    var query = "SELECT * FROM ?? WHERE ?? = ?";
+	    var table = ['photo_expresso.masque', 'STATUS', '0'];
 	    
 	    query = mysql.format(query, table);
 	    connection.query(query, function(err, result){
-		if (err) {
-		    res.status(400);
-		}
-		else {
+		if (err)
+		    res.status(400);		
+		else 
 		    res.send(result);
-		}
 	    });
 	});
 }
