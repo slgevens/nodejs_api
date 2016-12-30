6f20981f05358f8cf50bb0b2793f79f98db94629
Evens <evenssolignac@live.fr>
2016-12-30 15:40:05 +0100

    forget route can now send mail if you forget your password

--- app/routes/forgetRoute.js
+++ app/routes/forgetRoute.js
@@ -31,65 +31,39 @@ module.exports = function(router, connection) {
 		else {
 		    if(result.length != 0) {
 			var generatedPass = randompass(12);
-			console.log(generatedPass);
 			var hash =  pass.generate(generatedPass);
-			
 			var transporter = nodemailer.createTransport({
-			    service: 'Gmail',
-			    auth: {
+			    service: 'Gmail', auth: {
 				user: 'fsocietyestiam@gmail.com',
 				pass: 'lewegudrom'
 			    }
 			});
-			var text = pass.verify(generatedPass, hash );
-
-
-
-			
-			transporter.sendMail(mailOptions, function(err, info){
-			    if(err){
-				console.log(err);
-				res.json({ yo: 'err'});
-			    } else {
-				console.log('Message sent: '+ info.response);
-				res.json({yo: info.response});
-			    };
-			});
-			console.log(pass.verify(generatedPass, hash ));
-
 			var mailOptions = {
 			    from: 'fsocietyestiam.com',
 			    to: req.body.email,
-			    subject : 'Here is your new password'
-			    html: '<h4>Hello !</h4> <br/><br/><br/> <b>Here is your new password : </b>' + 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+			    subject : 'Here is your new password',
+			    html: '<h4>Hello !</h4> <br/><b>Here is your new password : </b><b>' + generatedPass + '</b></b> <p>Have a great day </p></br>--</br><h4>Fsociety Team</h4>'
+			};
 			var update_passsword = "UPDATE ?? SET ?? = ? WHERE ?";
 			var table_update = ['photo_expresso_v1.login', 'PASSWORD', pass.generate(generatedPass), result[0]];
 			update_passsword = mysql.format(update_passsword, table_update);
 			connection.query(update_passsword, function(err){
-			    if(err)
+			    if(err) {
 				res.status(400);
-			    else 
-				res.status(201).send("Password reset !");
-			});
-			    
-			} else {
-			    res.status(404).send("Nothing to show");
+			    }else {
+				transporter.sendMail(mailOptions, function(err, info){
+				    if(err) {
+					res.status(400);
+				    } else {
+					res.send("Password reset !");
+				    }
+				});
+			    }
+			});		
+		    } else {
+			res.status(404).send("Nothing to show");
 		    }
 		}
 	    });
-	})
+	})    
 };
