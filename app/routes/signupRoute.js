f6fd3b1ff43bbc3b0bad6bfbc8fd7ad32a4db1c1
Evens <evenssolignac@live.fr>
2017-01-18 14:42:46 +0100

    return instead of else conditon

--- app/routes/signupRoute.js
+++ app/routes/signupRoute.js
@@ -12,38 +12,40 @@ module.exports = function(router, connection) {
 	    connection.query(querySelect, function(err, resultSelect){
 		if (err) {
 		    res.status(400).send(err);
+		    return;
 		}
-		else if (resultSelect.length != 0) {
+		if (resultSelect.length != 0) {
 		    res.status(404).send("Compte existant !");
+		    return;
 		}
-		else {
-		    var queryInsert = "INSERT INTO ?? (??, ??, ??) VALUES (?, ?, ?)";
-		    var tableInsert = ['photo_expresso_v1.login', 'MAIL', 'PASSWORD', 'IS_ARCHIVED',
+		var queryInsert = "INSERT INTO ?? (??, ??, ??) VALUES (?, ?, ?)";
+		var tableInsert = ['photo_expresso_v1.login', 'MAIL', 'PASSWORD', 'IS_ARCHIVED',
 				   req.body.email, pass.generate(req.body.password), '0'];
+		
+		queryInsert = mysql.format(queryInsert, tableInsert);
+		connection.query(queryInsert, function(err, resultInsert){
+		    if (err) {
+			res.status(400).send(err);
+			return;
+		    }
 		    
-		    queryInsert = mysql.format(queryInsert, tableInsert);
-		    connection.query(queryInsert, function(err, resultInsert){
+		    var queryInsertUser = "INSERT INTO ?? (??, ??, ??, ??, ??, ??, ??, ??, ??, ??) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		    var tableInsertUSer = ['photo_expresso_v1.users', 'FIRSTNAME', 'LASTNAME', 'ADDR_L1',
+					   'ADDR_L2', 'COMPLEMENT', 'POSTAL_CODE', 'CITY', 'ID_MASK', 'ID_PAPER', 'ID_USER',
+					   req.body.firstname, req.body.lastname, req.body.addr_l1, req.body.addr_l2,
+					   req.body.complement, req.body.postal_code, req.body.city, req.body.id_masque,
+					   req.body.id_paper, resultInsert.insertId];
+		    queryInsertUser = mysql.format(queryInsertUser, tableInsertUSer);
+		    connection.query(queryInsertUser, function(err){
+			
 			if (err) {
 			    res.status(400).send(err);
-			}
-			else {			    
-			    var queryInsertUser = "INSERT INTO ?? (??, ??, ??, ??, ??, ??, ??, ??, ??, ??) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-			    var tableInsertUSer = ['photo_expresso_v1.users', 'FIRSTNAME', 'LASTNAME', 'ADDR_L1',
-						   'ADDR_L2', 'COMPLEMENT', 'POSTAL_CODE', 'CITY', 'ID_MASK', 'ID_PAPER', 'ID_USER',
-						   req.body.firstname, req.body.lastname, req.body.addr_l1, req.body.addr_l2,
-						   req.body.complement, req.body.postal_code, req.body.city, req.body.id_masque,
-						   req.body.id_paper, resultInsert.insertId];
-			    queryInsertUser = mysql.format(queryInsertUser, tableInsertUSer);
-			    connection.query(queryInsertUser, function(err){
-				if (err)
-				    res.status(400).send(err);
-				else
-				    res.status(200);
-			    })			    
-			}
-		    })
-		    res.status(201).send("Registred");
-		}
+			    return;
+			}			
+			res.status(200);
+		    });		    
+		});
+		res.status(201).send("Registred");		
 	    });
 	})
 }
