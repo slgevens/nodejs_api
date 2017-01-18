bf6c0d074cbf07c63bb63f080982e1a0a354675d
Evens <evenssolignac@live.fr>
2017-01-18 14:29:07 +0100

    return instead of else conditon

--- app/routes/forgetRoute.js
+++ app/routes/forgetRoute.js
@@ -27,43 +27,42 @@ module.exports = function(router, connection) {
 	    connection.query(query, function(err, result){
 		if (err) {
 		    res.status(400);
-		}		
-		else {
-		    if(result.length != 0) {
-			var generatedPass = randompass(12);
-			var hash =  pass.generate(generatedPass);
-			var transporter = nodemailer.createTransport({
-			    service: 'Gmail', auth: {
-				user: 'fsocietyestiam@gmail.com',
-				pass: 'lewegudrom'
-			    }
-			});
-			var mailOptions = {
-			    from: 'Fsociety ESTIAM',
-			    to: req.body.email,
-			    subject : 'Here is your new password',
-			    html: "<h4>Hello !</h4> <br/><b>Here is your new password : </b><b>" + generatedPass + "</b><p> Don't forget to change it !</p> <p>Have a great day </p>--</br><h4>Fsociety Team</h4>"
-			};
-			var update_passsword = "UPDATE ?? SET ?? = ? WHERE ?";
-			var table_update = ['photo_expresso_v1.login', 'PASSWORD', pass.generate(generatedPass), result[0]];
-			update_passsword = mysql.format(update_passsword, table_update);
-			connection.query(update_passsword, function(err){
+		    return;
+		}				
+		if(result.length != 0) {
+		    var generatedPass = randompass(12);
+		    var hash =  pass.generate(generatedPass);
+		    var transporter = nodemailer.createTransport({
+			service: 'Gmail', auth: {
+			    user: 'fsocietyestiam@gmail.com',
+			    pass: 'lewegudrom'
+			}
+		    });
+		    var mailOptions = {
+			from: 'Fsociety ESTIAM',
+			to: req.body.email,
+			subject : 'Here is your new password',
+			html: "<h4>Hello !</h4> <br/><b>Here is your new password : </b><b>" + generatedPass + "</b><p> Don't forget to change it !</p> <p>Have a good day </p>--</br><h4>Fsociety Team</h4>"
+		    };
+		    var update_passsword = "UPDATE ?? SET ?? = ? WHERE ?";
+		    var table_update = ['photo_expresso_v1.login', 'PASSWORD', pass.generate(generatedPass), result[0]];
+		    update_passsword = mysql.format(update_passsword, table_update);
+		    connection.query(update_passsword, function(err){
+			if(err) {
+			    res.status(400);
+			    return;
+			}		
+			transporter.sendMail(mailOptions, function(err, info){
 			    if(err) {
 				res.status(400);
-			    }else {
-				transporter.sendMail(mailOptions, function(err, info){
-				    if(err) {
-					res.status(400);
-				    } else {
-					res.send("Password reset !");
-				    }
-				});
+				return;
 			    }
-			});		
-		    } else {
-			res.status(404).send("Nothing to show");
-		    }
-		}
+			    res.send("Password reset !");			
+			});		    
+		    });
+		    return;
+		} 
+		res.status(404).send("Nothing to show");		
 	    });
-	})    
+	})  
 };
